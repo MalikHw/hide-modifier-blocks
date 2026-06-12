e4b1cca4e6e12b03209b9c71267566788b391b2a
MalikHw <help.malicorporation@gmail.com>
2026-06-12 22:53:54 +0100

    store pointers

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,23 @@ using namespace geode::prelude;
 // WHY
 static const std::vector<int> s_hideObjectIDs = {1755, 1813, 1829, 1859, 2866};
 
-static std::unordered_set<int> s_hiddenUniqueIDs;
+static std::unordered_set<GameObject*> s_hiddenObjects;
 
 class $modify(GameObject) { // :heart:
     void setVisible(bool visible) {
-        if (s_hiddenUniqueIDs.contains(this->m_uniqueID)) {GameObject::setVisible(false); return;}
+        if (s_hiddenObjects.contains(this)) {GameObject::setVisible(false); return;}
         GameObject::setVisible(visible);
     }
     void setOpacity(unsigned char opacity) {
-        if (s_hiddenUniqueIDs.contains(this->m_uniqueID)) {GameObject::setOpacity(0); return;}
+        if (s_hiddenObjects.contains(this)) {GameObject::setOpacity(0); return;}
         GameObject::setOpacity(opacity);
     }
 };
 
 class $modify(LevelEditorLayer) {
     struct Fields {
-        std::unordered_map<int, bool> prevInvisible;
-        std::unordered_map<int, bool> prevVisible;
+        std::unordered_map<GameObject*, bool> prevInvisible;
+        std::unordered_map<GameObject*, bool> prevVisible;
     };
 
     void hideMatchingObjects() {
@@ -37,31 +37,25 @@ class $modify(LevelEditorLayer) {
         for (auto obj : objs->asExt<GameObject*>()) {
             int oid = obj->m_objectID;
             if (std::find(s_hideObjectIDs.begin(), s_hideObjectIDs.end(), oid) != s_hideObjectIDs.end()) {
-                int uid = obj->m_uniqueID;
-                if (m_fields->prevInvisible.find(uid) == m_fields->prevInvisible.end()) {
-                    // save previous invisible state
-                    m_fields->prevInvisible[uid] = obj->m_isInvisible;
-                    m_fields->prevVisible[uid] = obj->isVisible();
+                if (m_fields->prevInvisible.find(obj) == m_fields->prevInvisible.end()) {
+                    m_fields->prevInvisible[obj] = obj->m_isInvisible;
+                    m_fields->prevVisible[obj] = obj->isVisible();
                 }
                 // mark invisible for editor/playtest handling
                 obj->m_isInvisible = true;
                 obj->setVisible(false);
                 obj->setOpacity(0);
-                s_hiddenUniqueIDs.insert(uid);
+                s_hiddenObjects.insert(obj);
             }
         }
     }
 
     void restoreSavedObjects() {
-        // restore objs by their id
-        for (auto it = m_fields->prevInvisible.begin(); it != m_fields->prevInvisible.end(); ++it) {
-            int uid = it->first;
-            auto obj = this->findGameObject(uid);
-            if (!obj) continue;
-            obj->m_isInvisible = it->second;
-            s_hiddenUniqueIDs.erase(uid);
-            if (m_fields->prevVisible.contains(uid)) {
-                obj->setVisible(m_fields->prevVisible[uid]);
+        for (auto& [obj, invisible] : m_fields->prevInvisible) {
+            obj->m_isInvisible = invisible;
+            s_hiddenObjects.erase(obj);
+            if (m_fields->prevVisible.contains(obj)) {
+                obj->setVisible(m_fields->prevVisible[obj]);
             }
         }
 
